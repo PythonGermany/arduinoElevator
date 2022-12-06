db69e67d9ed995dfa1658ef6ed65127b838419c2
PythonGermany <pythongermany@gmail.com>
2022-12-06 15:46:19 +0100

    Code shortened and changed to be more dynamic

--- arduinoAufzug.ino
+++ arduinoAufzug.ino
@@ -1,89 +1,55 @@
-int requestP1 = 2;
-int requestP2 = 3;
-int requestP3 = 4;
-int requestP4 = 5;
-
-int sensorF1 = 8;
-int sensorF2 = 9;
-int sensorF3 = 10;
-int sensorF4 = 11;
-int sensorD = 12;
-
-int powerUp = 7;
-int powerDown = 6;
+int floors = 4; 
+int requestStartP = 2; //Pins = requestStartP to requestStartP + floors - 1
+int powerP = 6; //Pins = powerP and powerP + 1
+int sensorStartP = 8; //Pins = sensorStartP to sensorStartP + floors - 1
+int sensorDoor = 12;
 
 int locNow = 0;
 int locStop = 0;
 bool active = false;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(requestP1, INPUT_PULLUP);
-    pinMode(requestP2, INPUT_PULLUP);
-    pinMode(requestP3, INPUT_PULLUP);
-    pinMode(requestP4, INPUT_PULLUP);
-
-    pinMode(sensorF1, INPUT_PULLUP);
-    pinMode(sensorF2, INPUT_PULLUP);
-    pinMode(sensorF3, INPUT_PULLUP);
-    pinMode(sensorF4, INPUT_PULLUP);
-    pinMode(sensorD, INPUT_PULLUP);
-
-    pinMode(powerDown, OUTPUT);
-    pinMode(powerUp, OUTPUT);
+    //Serial.begin(9600);
+    for (int i = requestStartP; i < requestStartP + floors; i++)
+        pinMode(i, INPUT_PULLUP);
+    for (int i = sensorStartP; i < sensorStartP + floors; i++)
+        pinMode(i, INPUT_PULLUP);
+    pinMode(sensorDoor, INPUT_PULLUP);
+    pinMode(powerP, OUTPUT);
+    pinMode(powerP + 1, OUTPUT);
 }
 
 void loop() {
-    checkLoc();
+    locNow = checkLoc();
     if (locStop && locNow) {
-        if (locStop == locNow || digitalRead(sensorD)) {
-            Serial.println("Stop floor reached or door opened");
-            digitalWrite(powerUp, LOW);
-            digitalWrite(powerDown, LOW);
+        if (locStop == locNow || digitalRead(sensorDoor)) {
+            //Serial.println("Stop floor reached or door open");
+            digitalWrite(powerP, LOW);
+            digitalWrite(powerP + 1, LOW);
             active = false;
             locStop = 0;
         }
-        else if (!active && !digitalRead(sensorD))
+        else if (!active)
         {
-            if (locStop > locNow) {
-                Serial.println("Going up");
-                digitalWrite(powerUp, HIGH);
-                active = true;
-            }
-            else if (locStop < locNow) {
-                Serial.println("Going down");
-                digitalWrite(powerDown, HIGH);
-                active = true;
-            }
+            digitalWrite(powerP + (locStop > locNow), HIGH); //if (locStop > locNow) {Serial.println("Going up");} else {Serial.println("Going down");}
+            active = true;
         }
     }
     else
-        checkRequest();
-    Serial.print(locNow); Serial.print(locStop); Serial.println(active);
-    //delay(1000);
+        locStop = checkRequest();
+    //Serial.print(locNow); Serial.print(locStop); Serial.print(active); Serial.println(!digitalRead(sensorDoor)); delay(1000);
 }
 
-void checkLoc() {
-    if (!digitalRead(sensorF1))
-        locNow = 1;
-    if (!digitalRead(sensorF2))
-        locNow = 2;
-    if (!digitalRead(sensorF3))
-        locNow = 3;
-    if (!digitalRead(sensorF4))
-        locNow = 4;
+int checkLoc() {
+    for (int i = sensorStartP; i < sensorStartP + floors; i++)
+        if (!digitalRead(i))
+            return (i - sensorStartP + 1);
+    return (locNow);
 }
 
-void checkRequest() {
-    if (!digitalRead(sensorD))
-    {
-        if (!digitalRead(requestP1))
-            locStop = 1;
-        else if (!digitalRead(requestP2))
-            locStop = 2;
-        else if (!digitalRead(requestP3))
-            locStop = 3;
-        else if (!digitalRead(requestP4))
-            locStop = 4;
-    }
+int checkRequest() {
+    for (int i = requestStartP; i < requestStartP + floors; i++)
+        if (!digitalRead(i))
+            return (i - requestStartP + 1);
+    return (locStop);
 }
