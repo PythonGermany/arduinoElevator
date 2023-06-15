323a7e96730a5f9c43bfddae3b3b755201828055
PythonGermany <>
2023-06-15 16:28:31 +0200

    Experimental changes

--- arduinoAufzug.ino
+++ arduinoAufzug.ino
@@ -8,7 +8,7 @@
 //It will cause the floor sensors to activate when theres a malfunction and the program will 
 //check the validity of activated sensors, (<=1 sensors active is valid) and. Doing that it will block and stop everything.
 
-const int floors = 4;
+const int floorCount = 4;
 const int requestStartP = 2; // Pins = requestStartP to requestStartP + floors - 1
 const int powerP = 6;		 // Pins = powerP and powerP + 1
 const int sensorStartP = 8;	 // Pins = sensorStartP to sensorStartP + floors - 1
@@ -16,14 +16,14 @@ const int sensorDoor = 12;
 
 int locNow = 0;
 int locStop = 0;
+bool active = false;
 bool validInput = false;
 
 void setup()
 {
-	//Serial.begin(9600);
-	for (int i = requestStartP; i < requestStartP + floors; i++)
+	for (int i = requestStartP; i < requestStartP + floorCount; i++)
 		pinMode(i, INPUT_PULLUP);
-	for (int i = sensorStartP; i < sensorStartP + floors; i++)
+	for (int i = sensorStartP; i < sensorStartP + floorCount; i++)
 		pinMode(i, INPUT_PULLUP);
 	pinMode(sensorDoor, INPUT_PULLUP);
 	pinMode(powerP, OUTPUT);
@@ -32,49 +32,36 @@ void setup()
 
 void loop()
 {
-	locNow = checkLoc();
-	validInput = sensors_valid(sensorStartP);
-	if (locStop && (locStop == locNow || digitalRead(sensorDoor)) || !validInput)
+	locNow = updateState(sensorStartP, floorCount, locNow);
+	locStop = updateState(requestStartP, floorCount, locStop);
+	validInput = sensors_valid(sensorStartP, floorCount) 
+		&& sensors_valid(requestStartP, floorCount);
+	if ((active && locStop == locNow) || !validInput || digitalRead(sensorDoor))
 	{
-		//Serial.print(millis()); Serial.println(" Stop floor reached or door open");
-		if (locNow == 1 && validInput)
-			delay(1000);
-		//Serial.print(millis()); Serial.println(" Motor stopped");
 		digitalWrite(powerP, LOW);
 		digitalWrite(powerP + 1, LOW);
-		locStop = 0;
+    	active = false;
 	}
-	else if (!locStop && locNow && validInput && !digitalRead(sensorDoor))
+	else if (!active && locStop != locNow && validInput && !digitalRead(sensorDoor))
 	{
-		locStop = checkRequest();
-		if (locStop)
-		  digitalWrite(powerP + (locStop > locNow), HIGH); //Serial.print(locNow); Serial.print(locStop); Serial.println(!digitalRead(sensorDoor)); if (locStop > locNow) {Serial.println("Going up");} else {Serial.println("Going down");}}
+		digitalWrite(powerP + (locStop > locNow), HIGH);
+		active = true;
 	}
 }
 
-bool sensors_valid(int startPin)
+bool sensors_valid(int start, int floors)
 {
 	int activated = 0;
-	for (int i = startPin; i < startPin + floors; i++)
+	for (int i = start; i < start + floors; i++)
 		if (!digitalRead(i))
 			activated++;
-	if (activated > 1)
-		return (false);
-	return (true);
+	return (activated <= 1);
 }
 
-int checkLoc()
+int updateState(int start, int floors, int prev)
 {
-	for (int i = sensorStartP; i < sensorStartP + floors; i++)
+	for (int i = start; i < start + floors; i++)
 		if (!digitalRead(i))
-			return (i - sensorStartP + 1);
-	return (locNow);
-}
-
-int checkRequest()
-{
-	for (int i = requestStartP; i < requestStartP + floors; i++)
-		if (!digitalRead(i) && i - requestStartP + 1 != locNow)
-			return (i - requestStartP + 1);
-	return (0);
+			return (i - start + 1);
+	return (prev);
 }
