fb657105491d787fb93f985b5e978c16a81fd670
PythonGermany <>
2023-06-27 03:17:44 +0200

    Moved debug function into seperate file

--- debug.hpp
+++ debug.hpp
@@ -1,7 +1,20 @@
 #ifndef DEBUG_HPP
 #define DEBUG_HPP
 
-// #define DEBUG
+#include <Arduino.h>
+
+#define DEBUG
 #define DEBUGINTERVAL 2500
+
+class Inputs;
+class Led;
+class Motor;
+
+void printDebug(Motor &motor, Led &ledStrip, int8_t &locNow, int8_t &locStop,
+                Inputs &manual, bool &error, Inputs &sensor, Inputs &request);
+
+#include "Inputs.hpp"
+#include "Led.hpp"
+#include "Motor.hpp"
 
 #endif
