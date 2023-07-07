3ccf302bc55f26c03736e5ec952a9d26278c3067
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:48:44 +0000

    Improved memory read/write implementation

--- Memory.cpp
+++ Memory.cpp
@@ -13,51 +13,58 @@ Memory::Memory(uint16_t size, uint16_t currAddress, uint8_t redundancy,
 Memory::~Memory() {}
 
 bool Memory::init(unsigned long seed, bool first) {
+  bool error = false;
   if (first) {
     randomSeed(seed);
-    uint16_t rand = random(size_);
-    id_ = rand;
-    for (uint8_t i = 0; i < 2; i++)
-      for (uint8_t j = 0; j < redundancy_; j++)
-        EEPROM.update(address_ + i * redundancy_ + j, (id_ >> (8 * i)));
-    uint16_t currAddress = dataAddress_ + id_ * redundancy_;
-    for (uint8_t i = 0; i < redundancy_; i++)
-      EEPROM.update(currAddress + i, -1);
+    id_ = random(size_);
+    writeAt(-2, id_ & 0xFF);
+    writeAt(-1, (id_ >> 8) & 0xFF);
+    writeAt(id_, -1);
   } else
-    for (uint8_t i = 0; i < 2; i++)
-      id_ += EEPROM.read(address_ + i * redundancy_) << (8 * i);
-  return false;
+    id_ = readAt(-2, error) + (readAt(-1, error) << 8);
+  return error;
 }
 
-int8_t Memory::read(bool &error) {
-  uint16_t currAddress = dataAddress_ + id_ * redundancy_;
-  for (uint8_t i = 0; i < redundancy_ - 1; i++) {
-    uint8_t data = EEPROM.read(currAddress + i);
-    uint8_t count = 1;
-    for (uint8_t j = i + 1; j < redundancy_; j++)
-      if (data == EEPROM.read(currAddress + j)) count++;
-    if (count >= redundancy_ / 2 + 1) {
-      error = false;
-      return data;
-    }
-  }
-  error = true;
-  return ERROR;
-}
+int8_t Memory::read(bool &error) { return readAt(id_, error); }
 
 void Memory::write(int8_t data) {
   if (++saveCount_ > size_) {
     saveCount_ = 1;
     id_ + 1 >= size_ ? id_ = 0 : id_++;
-    for (int i = 0; i < 2; i++)
-      EEPROM.update(address_ + i, (id_ >> (8 * i)) & 0xFF);
+    writeAt(-2, id_ & 0xFF);
+    writeAt(-1, (id_ >> 8) & 0xFF);
   }
-  uint16_t currAddress = dataAddress_ + id_ * redundancy_;
+  writeAt(id_, data);
+}
+
+int8_t Memory::readAt(int16_t id, bool &error) {
+#ifdef DEBUG
+  Serial.print("READ:    ");
+  Serial.println("Memory read at id: " + String(id));
+#endif
+  uint16_t currAddress = dataAddress_ + id * redundancy_;
+  for (uint8_t i = 0; i < redundancy_ - 1; i++) {
+    uint8_t data = EEPROM.read(currAddress + i);
+    uint8_t count = 1;
+    for (uint8_t j = i + 1; j < redundancy_; j++)
+      if (data == EEPROM.read(currAddress + j)) count++;
+    if (count >= redundancy_ / 2 + 1) return data;
+  }
+  error = true;
+#ifdef DEBUG
+  Serial.print("READ:    ");
+  Serial.println("Memory read error");
+#endif
+  return ERROR;
+}
+
+void Memory::writeAt(int16_t id, int8_t data) {
+  uint16_t currAddress = dataAddress_ + id * redundancy_;
   for (uint8_t i = 0; i < redundancy_; i++)
     EEPROM.update(currAddress + i, data);
 #ifdef DEBUG
   Serial.print("WRITE:   ");
-  Serial.print("Memory write at id: " + String(id_));
+  Serial.print("Memory write at id: " + String(id));
   Serial.print(" with data: " + String(data));
   Serial.println(" save count: " + String(saveCount_));
 #endif
