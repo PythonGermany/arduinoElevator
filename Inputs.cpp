be52403cde2492fa526a09af97ac811327e9412d
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:48:44 +0000

    Fixed blinking bugs and added emergency button

--- Inputs.cpp
+++ Inputs.cpp
@@ -2,34 +2,38 @@
 
 Inputs::Inputs(byte startPin, byte inputCount, bool invert = false)
     : startPin_(startPin), inputCount_(inputCount), invert_(invert) {
-  last_ = NONE;
-  saveAddress_ = NONE;
-  save_ = NONE;
   init();
 }
 
 Inputs::~Inputs() {}
 
 void Inputs::init() {
+  last_ = NONE;
+  saveAddress_ = NONE;
+  save_ = NONE;
+  saveCount_ = 0;
   for (byte i = 0; i < inputCount_; i++) {
     pinMode(startPin_ + i, INPUT_PULLUP);
   }
 }
 
 int8_t Inputs::update(bool lastPressed = false) {
-  static int16_t saveCount = 0;
   for (int8_t i = 0; i < inputCount_; i++) {
     if (digitalRead(startPin_ + i) == invert_) {
       if (save_ != NONE && i != last()) {
-        if (++saveCount >= SAVESPERADDRESS) {
+        if (++saveCount_ >= SAVESPERADDRESS) {
           save_ + 1 >= EEPROM.length() ? save_ = saveAddress_ + 1 : save_++;
           EEPROM.write(saveAddress_, save_);
-          saveCount = 0;
+          saveCount_ = 0;
+#ifdef DEBUG
           Serial.println("SAVE LOC UPDATED TO " + String(save_));
+#endif
         }
         EEPROM.write(save_, i);
+#ifdef DEBUG
         Serial.print("NEW VALUE: '" + String(i) + "' SAVED");
         Serial.println(" AT LOCATION " + String(save_));
+#endif
       }
       last_ = i;
       return i;
@@ -50,6 +54,6 @@ int8_t Inputs::last() const { return last_; }
 void Inputs::setSaveAddress(int16_t saveAddress) {
   saveAddress_ = saveAddress;
   save_ = EEPROM.read(saveAddress_);
-  if (save_ == saveAddress_) save_ = saveAddress_ + 1;
+  if (save_ == saveAddress_ || save_ == 0) save_ = saveAddress_ + 1;
   last_ = EEPROM.read(save_);
 }
