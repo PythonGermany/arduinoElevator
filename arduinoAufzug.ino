1806a670eda0d59a980cb0da285498ed3623804a
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-07-07 15:48:44 +0000

    Sensor connection comment added

--- arduinoAufzug.ino
+++ arduinoAufzug.ino
@@ -1,3 +1,13 @@
+//In case of a sensor malfunction (e. g. loose connection) 
+//the door sensor needs to be in the 'door open' state so door open means sensor circuit open.
+//For the floor sensors in case of malfunction they also should be seen as activated when there is no connection.
+//The request sensors should be activated when the circuit is closed so there is no request in case of malfunction.
+
+//So for floor and door sensors no connection always means the sensor is being triggered.
+//It will lead the door sensor to block or stop everything in case of malfunction.
+//It will cause the floor sensors to activate when theres a malfunction and the program will 
+//check the validity of activated sensors, (<=1 sensors active is valid) and. Doing that it will block and stop everything.
+
 const int floors = 4;
 const int requestStartP = 2; // Pins = requestStartP to requestStartP + floors - 1
 const int powerP = 6;		 // Pins = powerP and powerP + 1
@@ -10,7 +20,7 @@ bool validInput = false;
 
 void setup()
 {
-	 Serial.begin(9600);
+	//Serial.begin(9600);
 	for (int i = requestStartP; i < requestStartP + floors; i++)
 		pinMode(i, INPUT_PULLUP);
 	for (int i = sensorStartP; i < sensorStartP + floors; i++)
@@ -26,10 +36,10 @@ void loop()
 	validInput = sensors_valid(sensorStartP);
 	if (locStop && (locStop == locNow || digitalRead(sensorDoor)) || !validInput)
 	{
-		Serial.print(millis()); Serial.println(" Stop floor reached or door open");
+		//Serial.print(millis()); Serial.println(" Stop floor reached or door open");
 		if (locNow == 1 && validInput)
 			delay(1000);
-		Serial.print(millis()); Serial.println(" Motor stopped");
+		//Serial.print(millis()); Serial.println(" Motor stopped");
 		digitalWrite(powerP, LOW);
 		digitalWrite(powerP + 1, LOW);
 		locStop = 0;
@@ -38,7 +48,7 @@ void loop()
 	{
 		locStop = checkRequest();
 		if (locStop)
-		  {digitalWrite(powerP + (locStop > locNow), HIGH); Serial.print(locNow); Serial.print(locStop); Serial.println(!digitalRead(sensorDoor)); if (locStop > locNow) {Serial.println("Going up");} else {Serial.println("Going down");}}
+		  digitalWrite(powerP + (locStop > locNow), HIGH); //Serial.print(locNow); Serial.print(locStop); Serial.println(!digitalRead(sensorDoor)); if (locStop > locNow) {Serial.println("Going up");} else {Serial.println("Going down");}}
 	}
 }
 
